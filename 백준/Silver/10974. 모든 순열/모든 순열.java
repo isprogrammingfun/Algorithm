1ce0e5e99dc524c6631dbd6519d388c68651a4c3
seeun <78543382+isprogrammingfun@users.noreply.github.com>
2022-10-10 02:21:35 +0900

    [Silver III] Title: 모든 순열, Time: 2128 ms, Memory: 62404 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+
+	private static int N;
+	private static int arr[];
+	private static boolean isSelected[];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		isSelected = new boolean[N];
+		
+		perm(0);
+	}
+	
+	private static void perm(int cnt) {
+		if(cnt == N) {
+			for(int i=0;i<N;i++)
+				System.out.print(arr[i]+" ");
+			System.out.println();
+			return ;
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(isSelected[i])
+				continue;
+			arr[cnt] = i+1;
+			isSelected[i] = true;
+			perm(cnt+1);
+			isSelected[i] = false;
+		}
+	}
+
+}
