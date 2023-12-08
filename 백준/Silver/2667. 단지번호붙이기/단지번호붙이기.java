b9604cf96ceee02c5775cd5f7ff9770480f9b788
seeun <78543382+isprogrammingfun@users.noreply.github.com>
2023-12-08 23:38:53 +0900

    [Silver I] Title: 단지번호붙이기, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,63 +2,58 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int[][] arr;
-	static boolean[][] visited;
-	static int[] dx = {0,0,-1,1};
-	static int[] dy = {-1,1,0,0};
-	static List<Integer> result;
-	static int cnt, N;
-
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		N = Integer.parseInt(br.readLine());
+    static int[][] arr;
+    static boolean[][] visited;
+    static int[] dx = { -1, 1, 0, 0 }; 
+    static int[] dy = { 0, 0, -1, 1 }; 
+    static List<Integer> result;
+    static int cnt,N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
         
-        result = new LinkedList<>(); 
-		arr = new int[N][N];
-		visited = new boolean[N][N];
-		cnt = 1;
-		
-		for(int i = 0;i < N;i++) {
-			String s = br.readLine();
-			for(int j = 0;j < N;j++) {
-				arr[i][j] = s.charAt(j)-'0';
-			}
-		}
-		
-		
-		for(int x = 0;x < N;x++) {
-			for(int y = 0;y < N;y++) {
-				if(arr[x][y]==1 && !visited[x][y]) {
-					dfs(x,y);
-					result.add(cnt);
+        arr = new int[N][N];
+        visited = new boolean[N][N];
+        cnt = 1;
+        result = new LinkedList<>();
+        
+        for(int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for(int j = 0; j < N; j++) {
+                arr[i][j] = s.charAt(j) - '0';
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 1 && !visited[i][j]) {
+                    dfs(i,j);
+                    result.add(cnt);
 					cnt = 1;
-				}
-			}
-		}
-		
-		Collections.sort(result);
+                }
+            }
+        }
+        
+        Collections.sort(result);
         
         System.out.println(result.size());
         for(int r : result) {
             System.out.println(r);
         }
-
-	}
-	
-	public static void dfs(int x, int y) {
-		visited[x][y] = true;
-		
-		for(int i=0;i<4;i++) {
-			int nx = dx[i]+x;
-			int ny = dy[i]+y;
-            if (nx < 0 || ny < 0 || nx >= N || ny >= N)
+    }
+    
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for(int i=0; i<4; i++) {
+		   int nx = dx[i]+x;
+		   int ny = dy[i]+y;
+ 
+           if (nx < 0 || ny < 0 || nx >= N || ny >= N)
                continue;
-            if (visited[nx][ny] || arr[nx][ny] == 0)
+           if (visited[nx][ny] || arr[nx][ny] == 0)
                continue;
-            cnt++;
-            dfs(nx,ny);
-		}
-	}
+           cnt++;
+           dfs(nx,ny);
+        }
+    }
 }
