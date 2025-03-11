6c29c19634127ca54449fc3eb3b9f1cf5a69aa73
seeun <78543382+isprogrammingfun@users.noreply.github.com>
2025-03-11 17:00:46 +0900

    Time: 5 ms (87.35%), Space: 45 MB (31.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,26 +1,28 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int answer = 0;
-        for(int i = 0; i < s.length(); i++) {
-            for(int j = i + 1; j <= s.length(); j++) {
-                String str = s.substring(i, j);
-                boolean hasDuplicate = false;
-                
-                HashSet<Character> charSet = new HashSet<>();
-                for (char c : str.toCharArray()) {
-                    if (!charSet.add(c)) {
-                        hasDuplicate = true;
-                        break;
-                    }
-                }
-                
-                if (!hasDuplicate) {
-                    answer = Math.max(answer, str.length());
-                } else {
-                    break;
-                }
+        int n = s.length();
+        int maxLength = 0;
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        
+        // 윈도우의 시작 위치
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            char c = s.charAt(right);
+            
+            // 문자가 이미 존재하고, 현재 윈도우 내에 있으면
+            if (charMap.containsKey(c) && charMap.get(c) >= left) {
+                // 중복 문자 다음 위치로 left 이동
+                left = charMap.get(c) + 1;
             }
+            
+            // 현재 문자 위치 저장
+            charMap.put(c, right);
+            
+            // 최대 길이 갱신
+            maxLength = Math.max(maxLength, right - left + 1);
         }
-        return answer;
+        
+        return maxLength;
     }
 }
